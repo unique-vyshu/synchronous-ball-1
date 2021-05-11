29b09855e1e98a927cce348f6ad04bdfd09a0d85
unique-vyshu <74426792+unique-vyshu@users.noreply.github.com>
2021-05-11 19:52:44 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master (5)/asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,53 @@
+var hypnoticball;
+var database;
+var position;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    hypnoticball = createSprite(250,250,10,10);
+    hypnoticball.shapeColor = "red";
+
+    var hypniticballposition = database.ref('ball/position');
+    hypniticballposition.on("value",readPosition,showError)
+
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({'x' :position.x+x,'y':position.y+y })
+
+   
+}
+
+
+function readPosition(data){
+    position=data.val();
+    hypnoticball.x = position.x;
+    hypnoticball.y = position.y;
+
+
+
+}
+
+function showError(){
+    console.log ("Error in accessing the Database")
+    
+}
